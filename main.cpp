b035c7b788a333cb65e0567206a62a01bc516a47
Ricardo Martin Marcucci <mmarcucci@ucc.edu.ar>
2018-03-12 15:51:38 -0300

    Agregado ejemplo de numeros primos

--- main.cpp
+++ main.cpp
@@ -1,37 +1,61 @@
 #include <iostream>
 #include <thread>
+#include <vector>
 #include <ctime>
 
+using namespace std;
 
-void sleep( int s){
+void tarea(int inicio, int fin);
+
+void sleep(int s) {
     clock_t begin;
     begin = clock();
-    double elapsed_secs=0;
+    double elapsed_secs = 0;
 
-    while(elapsed_secs < s){
+    while (elapsed_secs < s) {
         elapsed_secs = double(clock() - begin) / CLOCKS_PER_SEC;
     }
 }
 
-void hola(){
+void hola() {
 
-    for(int i = 0; i< 5; i++) {
+    for (int i = 0; i < 5; i++) {
         std::cout << "Hola " << i << std::endl;
         sleep(2);
     }
 }
 
 
+bool esPrimo(unsigned int num) {
+    for (unsigned int i = 2; i <= (num / 2); i++)
+        if (num % i == 0)
+            return false;
+
+
+    return true;
+}
+
+#define CANT_HILOS 3
+
 int main() {
-    std::thread hilo(hola);
-    hilo.detach();
+    vector<thread> h;
+    unsigned int inicio = 2;
+    unsigned int fin = 300000;
+    unsigned int intervalo = (fin - inicio) / CANT_HILOS;
 
-    for(int i = 0; i< 10; i++){
-
-        std::cout << "Chau " << i << std::endl;
-        sleep(1);
+    for (int j = 0; j < CANT_HILOS; j++) {
+        h.emplace_back(tarea, inicio + intervalo * j, inicio + intervalo * (j + 1) - 1);
     }
+    for (auto &actual : h)
+        actual.join();
 
 
     return 0;
+}
+
+void tarea(int inicio, int fin) {
+    for (int i = inicio; i < fin; i++)
+        if (esPrimo(i))
+            std::cout << i << std::endl;
+
 }

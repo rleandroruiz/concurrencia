8ff2f79c2211509fa56ab9e7cef51900b30527fe
Ricardo Martin Marcucci <mmarcucci@ucc.edu.ar>
2018-03-12 18:30:16 -0300

    Agregada cola de mensajes

--- main.cpp
+++ main.cpp
@@ -2,28 +2,14 @@
 #include <thread>
 #include <vector>
 #include <ctime>
+#include <mutex>
 
 using namespace std;
 
+mutex pantalla;
+
+
 void tarea(int inicio, int fin);
-
-void sleep(int s) {
-    clock_t begin;
-    begin = clock();
-    double elapsed_secs = 0;
-
-    while (elapsed_secs < s) {
-        elapsed_secs = double(clock() - begin) / CLOCKS_PER_SEC;
-    }
-}
-
-void hola() {
-
-    for (int i = 0; i < 5; i++) {
-        std::cout << "Hola " << i << std::endl;
-        sleep(2);
-    }
-}
 
 
 bool esPrimo(unsigned int num) {
@@ -31,11 +17,10 @@ bool esPrimo(unsigned int num) {
         if (num % i == 0)
             return false;
 
-
     return true;
 }
 
-#define CANT_HILOS 3
+#define CANT_HILOS 30
 
 int main() {
     vector<thread> h;
@@ -43,19 +28,24 @@ int main() {
     unsigned int fin = 300000;
     unsigned int intervalo = (fin - inicio) / CANT_HILOS;
 
+
+
     for (int j = 0; j < CANT_HILOS; j++) {
         h.emplace_back(tarea, inicio + intervalo * j, inicio + intervalo * (j + 1) - 1);
     }
+
     for (auto &actual : h)
         actual.join();
-
 
     return 0;
 }
 
 void tarea(int inicio, int fin) {
-    for (int i = inicio; i < fin; i++)
-        if (esPrimo(i))
-            std::cout << i << std::endl;
 
+    for (int i = inicio; i < fin; i++)
+        if (esPrimo(i)) {
+            pantalla.lock();
+            std::cout << i << std::endl;
+            pantalla.unlock();
+        }
 }
---
+++ msg_queue.h
@@ -0,0 +1,42 @@
+//
+// Created by martin on 12/03/18.
+//
+
+#include <queue>
+#include <mutex>
+#include <condition_variable>
+
+#ifndef CONCURRENCY_MSG_QUEUE_H
+#define CONCURRENCY_MSG_QUEUE_H
+
+template<class T>
+class msg_queue {
+private:
+    std::queue<T> cola;
+    mutable std::mutex mut;
+    std::condition_variable cond;
+public:
+    msg_queue(void) : cola(), mut(), cond() {}
+
+    ~msg_queue(void) {}
+
+    void enqueue(T t) {
+        std::lock_guard<std::mutex> lock(mut);
+        cola.push(t);
+        cond.notify_one();
+    }
+
+    T dequeue(void) {
+        std::unique_lock<std::mutex> lock(mut);
+        while (cola.empty()) {
+            cond.wait(lock);
+        }
+        T val = cola.front();
+        cola.pop();
+        return val;
+    }
+
+};
+
+
+#endif //CONCURRENCY_MSG_QUEUE_H
